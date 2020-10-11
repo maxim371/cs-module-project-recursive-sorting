be68204e58b8d097418609aa07b050156d145a04
maxim371 <kwaku.dr@gmail.com>
2020-10-10 22:27:20 -0400

    recursive-sorting

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,25 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
     # Your code here
+    if end >= start:
+        mid = start + (end - start) // 2
+
+        # if mid found, return mid
+        if arr[mid] == target:
+            return mid
+
+        # search left half
+        elif arr[mid] > target:
+            return binary_search(arr, target, start, mid - 1)
+            # search left half
+        else: #arr[mid] < target
+            return binary_search(arr, target, mid + 1, end)
+
+    else:
+        return -1
+
+
+        
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -11,4 +30,37 @@ def binary_search(arr, target, start, end):
 # or iteratively
 def agnostic_binary_search(arr, target):
     # Your code here
+    start = 0
+    end = len(arr) - 1
+    if arr[start] == arr[end]:
+        if target == arr[start]:
+            return start
+        else:
+            return - 1
+    # boolean to keep track of array descending or ascending
+    is_ascending = arr[start] < arr[end]
+    # array ascending?, perform normal binary search
+    if is_ascending:
+        return binary_search(arr, target, 0, len(arr) - 1)
+    # array descending?, perfrom reverse binary search
+    else:
+        return descending_binary_search(arr, target, 0, len(arr) - 1)
+
+def descending_binary_search(arr, target, start, end):
+    if start > end:
+        return - 1
+    else:
+        mid = (start + end) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] > target:
+            return descending_binary_search(arr, target, mid + 1, end)
+        else:
+            return descending_binary_search(arr, target, start, mid - 1)
+
+array = [-1, 0, 1, 9, 10, 28]
+target = 9
+result = binary_search(array, target, 0, len(array) - 1)
+print(f'target index: {result}')
+
 
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,17 +1,31 @@
+from bisect import bisect_right as br, bisect_left as bl
+
 # TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
 
     # Your code here
-
-
+    for i in range(elements):
+        if not arrA:
+            merged_arr[i] = arrB.pop(0)
+        elif not arrB:
+            merged_arr[i] = arrA.pop(0)
+        elif arrA[0] < arrB[0]:
+            merged_arr[i] = arrA.pop(0)
+        elif arrB[0] < arrA[0]:
+            merged_arr[i] = arrB.pop(0)
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
     # Your code here
-
+    len_arr = len(arr)
+    if len_arr > 1:
+        split = len_arr // 2
+        left = merge_sort(arr[:split])
+        right = merge_sort(arr[split:])
+        arr = merge(left, right)
 
     return arr
 
@@ -19,10 +33,54 @@ def merge_sort(arr):
 # utilize any extra memory
 # In other words, your implementation should not allocate any additional lists 
 # or data structures; it can only re-use the memory it was given as input
-def merge_in_place(arr, start, mid, end):
-    # Your code here
+def merge_in_place(arr, start, mid, end):  
+    new = mid + 1
 
+    if arr[mid] <= arr[new]:
+        return
+
+    while start <= mid and new <= end:
+
+        if arr[start] <= arr[new]:
+            start += 1
+        else:
+            value = arr[new]
+            index = new
+
+            while index != start:
+                arr[index] = arr[index - 1]
+                index -= 1
+
+            arr[start] = value
+
+            start += 1
+            mid += 1
+            new += 1
 
 def merge_sort_in_place(arr, l, r):
-    # Your code here
+    if (r - l) > 1:
+
+        mid = 1 + (r - l) // 2
+        # sort first and second halfs
+        merge_sort_in_place(arr, l, mid)
+        merge_sort_in_place(arr, mid, r)
+        
+        merge_in_place(arr, l, mid, r)    
+
+
+# def printList(array):
+#     for ii in range(len(array)):
+#         print(array[ii], end=', ')
+
+# arrA = [0, 33, 44, 77]
+# arrB = [-1, 22, 77, 99]
+
+# _merge_ = merge(arrA, arrB)
+
+# array = [4, 2, 1, 0, 7, 5, 12, -4]
+# array_2 = [44, 33, 22, 0, 77, 55, 44, -11]
+
+# sorted_1 = merge_sort(array)
+# print('\n')
+
 
